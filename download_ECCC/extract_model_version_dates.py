fa1ac5620350bbe24bb38bf1231830208e820d2c
t2hsu <t2hsu@aha.ucsd.edu>
2024-03-06 12:56:17 -0800

    Create the correct model version date mapping

--- download_ECCC/extract_model_version_dates.py
+++ download_ECCC/extract_model_version_dates.py
@@ -9,7 +9,7 @@ date_fmt = re.compile(r'^\s*(\d{4}-\d{2}-\d{2})\s*$')
 
 with open(output_file, "w") as out_f:
     with open(raw_file, "r") as in_f:
-        for s in in_f:
+        for s in in_f.readlines():
             m = date_fmt.match(s)
             if m is not None:
                 print(m.group(1))
--- download_ECCC/tools.py
+++ download_ECCC/tools.py
@@ -6,13 +6,16 @@ model_version_date_bounds = dict(
     GEPS6 = [ pd.Timestamp("2019-07-24"), pd.Timestamp("2021-11-25"), ],
 )
 
-
+valid_start_time_bnds = dict(
+    GEPS5 = [ pd.Timestamp("1998-01-01"), pd.Timestamp("2017-12-31")],
+    GEPS6 = [ pd.Timestamp("1998-01-01"), pd.Timestamp("2017-12-31")],
+)
 
 
 # Load the data
 valid_model_version_dates = dict(GEPS5=[], GEPS6=[])
 with open("model_version_dates.txt", "r") as f:
-    for s in f:
+    for s in f.readlines():
         if s != "":
             ts = pd.Timestamp(s)
             for model_version in ["GEPS5", "GEPS6"]:
@@ -20,10 +23,14 @@ with open("model_version_dates.txt", "r") as f:
                 if ts >= bnds[0] and ts <= bnds[1]:
                     valid_model_version_dates[model_version].append(ts)
 
+            
+for model_version in ["GEPS5", "GEPS6"]:
+    valid_model_version_dates[model_version].sort(key=lambda ts: (ts.month, ts.day))
+    
 def printValidModelVersionDates():
      for model_version in ["GEPS5", "GEPS6"]:
         for i, model_version_date in enumerate(valid_model_version_dates[model_version]):
-            print("[%d] %s " % (i, model_version_date.strftime("%Y-%m-%d")))
+            print("[%s - %d] %s " % (model_version, i, model_version_date.strftime("%Y-%m-%d")))
 
 
 
@@ -34,7 +41,7 @@ def printValidModelVersionDates():
 
     The function returns model_version_date
 """
-def model_version_reforecast_date_mapping(model_version, reforecast_date):
+def modelVersionReforecastDateToModelVersionDate(model_version, reforecast_date):
     
     # IMPORTANT:
     # Notice that there is no two reforecast_date of the same model_version
@@ -43,12 +50,31 @@ def model_version_reforecast_date_mapping(model_version, reforecast_date):
     # Therefore, a specific month-day will map to a specific model_version_date
     # for either GEPS5 or GEPS6
 
-    if model_version == "GEPS5":
-        pass
-        #if reforecast_date in  
+    _valid_model_version_dates = valid_model_version_dates[model_version]
+    for i, valid_model_version_date in enumerate(_valid_model_version_dates):
+        if valid_model_version_date.month == reforecast_date.month and \
+           valid_model_version_date.day   == reforecast_date.day:
+            
+            return valid_model_version_date
+            
         
-        
+    return None
+
 if __name__ == "__main__":   
+
     printValidModelVersionDates()
 
+    test_dates = dict(
+        GEPS5 = ["2001-01-03", "2016-02-05", ],
+        GEPS6 = ["2012-09-08", "1998-05-06", ],
+    )
 
+    for model_version, _test_dates in test_dates.items():
+        for _test_date in _test_dates:
+            _test_date_ts = pd.Timestamp(_test_date)
+            print("[%s] Test the date %s maps to model version date " % (model_version, _test_date,), 
+                modelVersionReforecastDateToModelVersionDate(model_version, _test_date_ts),
+            )
+    
+    
+    
