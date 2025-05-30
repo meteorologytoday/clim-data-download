ff02cd4c7968970afb82435296219553538d8a18
Tien-Yiao Hsu <t2hsu@login01.expanse.sdsc.edu>
2025-05-30 10:28:08 -0700

    Add setup

---
+++ download_GHRSST/000_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+work_root=`pwd`
+
+export PYTHONPATH=$work_root/src/lib:$PYTHONPATH
+
+echo "PYTHONPATH = $PYTHONPATH"
+
+data_dir=/home/t2hsu/temp_project/data_project-SST-spectrum/data
+
+
+
+
